6f69007d191e1f2c7417967f77ff3721d5b27414
Daniel Lacasse <daniel@lacasse.io>
2018-07-21 13:09:59 +0300

    User worker API when generating HTML report

--- gradle-plugins-analysis/src/main/groovy/org/gradleplugins/tasks/GeneratePluginAnalysisDetailPage.java
+++ gradle-plugins-analysis/src/main/groovy/org/gradleplugins/tasks/GeneratePluginAnalysisDetailPage.java
@@ -23,9 +23,12 @@ import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.workers.IsolationMode;
+import org.gradle.workers.WorkerExecutor;
 import org.gradleplugins.AnalyzeReport;
 import org.yaml.snakeyaml.Yaml;
 
+import javax.inject.Inject;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.Map;
@@ -33,6 +36,14 @@ import java.util.Map;
 public class GeneratePluginAnalysisDetailPage extends DefaultTask {
     private final RegularFileProperty report = newInputFile();
     private final RegularFileProperty detailHtml = newOutputFile();
+
+    @Inject
+    public GeneratePluginAnalysisDetailPage() {}
+
+    @Inject
+    protected WorkerExecutor getWorkerExecutor() {
+        throw new UnsupportedOperationException();
+    }
 
     @InputFile
     public RegularFileProperty getReport() {
@@ -46,27 +57,46 @@ public class GeneratePluginAnalysisDetailPage extends DefaultTask {
 
     @TaskAction
     private void doGenerateDetail() {
-        AnalyzeReport r = null;
-        String prettyJSONString = null;
-        try (InputStream inStream = new FileInputStream(report.get().getAsFile())) {
-            prettyJSONString = IOUtils.toString(inStream, Charset.defaultCharset());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        getWorkerExecutor().submit(GenerateRunnable.class, (workerConfiguration) -> {
+            workerConfiguration.setIsolationMode(IsolationMode.NONE);
+            workerConfiguration.params(report.getAsFile().get(), detailHtml.getAsFile().get());
+        });
+    }
+
+    public static class GenerateRunnable implements Runnable {
+        private final File report;
+        private final File detailHtml;
+
+        @Inject
+        GenerateRunnable(File report, File detailHtml) {
+            this.report = report;
+            this.detailHtml = detailHtml;
         }
 
-        Yaml yaml = new Yaml();
+        @Override
+        public void run() {
+            AnalyzeReport r = null;
+            String prettyJSONString = null;
+            try (InputStream inStream = new FileInputStream(report)) {
+                prettyJSONString = IOUtils.toString(inStream, Charset.defaultCharset());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        Map<String,Object> map = (Map<String, Object>) yaml.load(prettyJSONString);
+            Yaml yaml = new Yaml();
 
-        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(detailHtml.getAsFile().get())))) {
-            out.println("---");
-            out.println("layout: plugin");
-            out.println(yaml.dump(map));
-            out.println("---");
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            Map<String,Object> map = (Map<String, Object>) yaml.load(prettyJSONString);
+
+            try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(detailHtml)))) {
+                out.println("---");
+                out.println("layout: plugin");
+                out.println(yaml.dump(map));
+                out.println("---");
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
         }
     }
 }
